6c5d03cdeaad9065f466687c9f0b8163943ec060
katarinap02 <148916553+katarinap02@users.noreply.github.com>
2025-10-21 14:26:14 +0200

    added some things on front page

--- src/app/home/home-logged/home-logged.component.ts
+++ src/app/home/home-logged/home-logged.component.ts
@@ -1,13 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { LocalCommunityService } from 'src/app/services/localcommunity.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home-logged',
   templateUrl: './home-logged.component.html',
   styleUrls: ['./home-logged.component.css'],
 })
-export class HomeLoggedComponent {
+export class HomeLoggedComponent implements OnInit{
 
 
 
@@ -17,11 +19,37 @@ export class HomeLoggedComponent {
   isModal4Open: boolean = false;
   isModalAccountOpen = false;
   userId: number = -1;
+  myCommunityName: string | null = null;
 
   isAdmin = false;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private userService: UserService, private communityService: LocalCommunityService) {
     this.checkRole();
+  }
+  ngOnInit(): void {
+    const userId = this.authService.getId();
+  if (!userId) {
+    console.error('Korisnik nije prijavljen.');
+    return;
+  }
+
+  this.userService.getById(Number(userId)).subscribe({
+    next: (user) => {
+      const communityId = user.localCommunityId;
+
+      if (communityId) {
+        this.communityService.getById(communityId).subscribe({
+          next: (community) => {
+            this.myCommunityName = community.name;
+          },
+          error: (err) => console.error('Greška pri učitavanju mesne zajednice:', err)
+        });
+      } else {
+        this.myCommunityName = 'Nema dodeljenu mesnu zajednicu';
+      }
+    },
+    error: (err) => console.error('Greška pri učitavanju korisnika:', err)
+  });
   }
 
   checkRole() {
@@ -70,6 +98,28 @@ this.router.navigate(['/announcement']);
       this.goToMyProfile();
     }
   }
+
+goToMyCommunity(): void {
+    const userId = this.authService.getId();
+    if (!userId) {
+      console.error('Korisnik nije prijavljen.');
+      return;
+    }
+
+    this.userService.getById(Number(userId)).subscribe({
+      next: (user) => {
+        const communityId = user.localCommunityId || -1 
+        console.log(communityId)
+        if (communityId || communityId != -1) {
+          this.router.navigate([`/view-community/${communityId}`]);
+        } else {
+          alert('Korisnik nema dodeljenu mesnu zajednicu.');
+        }
+      },
+      error: (err) => console.error('Greška pri učitavanju korisnika', err)
+    });
+  }
+
 
   
 }
